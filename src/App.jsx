73316fbda0b953d55eb968cffa2faf4b7e306a4b
gabyreis28 <gabriela.dias.97@hotmail.com>
2022-01-27 20:21:38 -0300

    style cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,29 @@ import ParametroComp from './components/basico/Parametro'
 import FragmentoComp from './components/basico/Fragmento'
 import Aleatorio from './components/basico/Aleatorio'
 import Card from './components/layout/Card'
+import './App.css'
 
-export default _ => 
-  <div>
+export default () => (
+  <div className="App">
     <h1>
-      Fundamentos React <br></br><br></br>
+      Fundamentos React
     </h1>
+    <div className="Cards">
+      <Card titulo="#004 - Desafio Aleatório">
+      <Aleatorio min={ 1 } max={ 60 }/>
+      </Card>
 
-    <Card titulo="#004 - Desafio Aleatório">
-     <Aleatorio min={ 1 } max={ 60 }/>
-    </Card>
+      <Card titulo="#003 - Fragmento">   
+        <FragmentoComp/>
+      </Card>
 
-    <Card titulo="#003 - Fragmento">   
-      <FragmentoComp/>
-    </Card>
+      <Card titulo="#002 - Desafio Com Parametro">
+        <ParametroComp titulo="Situação do Aluno" aluno='Pedro Silva' nota={ 8.3 }/>
+      </Card>
 
-    <Card titulo="#002 - Desafio Com Parametro">
-      <ParametroComp titulo="Situação do Aluno" aluno='Pedro Silva' nota={ 8.3 }/>
-    </Card>
-
-    <Card titulo="#001 -  Desafio Primeiro Componente">
-      <PrimeiroComp></PrimeiroComp>
-    </Card>
-
+      <Card titulo="#001 -  Desafio Primeiro Componente">
+        <PrimeiroComp></PrimeiroComp>
+      </Card>
+    </div>
   </div>
+)
